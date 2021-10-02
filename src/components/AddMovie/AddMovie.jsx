9048244429d8968e1fe1e04d51d0eb9e98ca90ba
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-02 10:00:01 -0400

    addmovie.jsx - added cancel button that calls function handleBack and save button that calls function addNewMovie. Invalid hook and dropdown cannot select genres

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -2,8 +2,10 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import { useHistory } from "react-router";
 
 function AddMovie() {
+    const history = useHistory();
     const dispatch = useDispatch();
     const genreList = useSelector(store => store.genres);
 
@@ -20,13 +22,28 @@ function AddMovie() {
         dispatch({type: 'GET_GENRES'});
     }
 
+    //function handleBack - clear inputs and return user to home
+    const handleBack = () => {
+        useState({title: '', poster: '', description: '', genres: []});
+        history.push("/");
+    }
+
+    //function addNewMovie - dispatch to call POST, clear inputs, return user to home
+    const addNewMovie = (event) => {
+        //prevent default form behavior
+        event.preventDefault();
+        dispatch({type: 'ADD_NEW_MOVIE', payload: newMovie});
+        useState({title: '', poster: '', description: '', genres: []});
+        history.push("/");
+    }
+
     return(
         <>
             {JSON.stringify(genreList)}
             <br/>
             {JSON.stringify(newMovie)}
             <h3>Add a new movie:</h3>
-            <form>
+            <form onSubmit={addNewMovie}>
                 <input required placeholder="Movie Title" type="text" value={newMovie.title} onChange={(event) => setNewMovie({...newMovie, title: event.target.value})} />
                 <input required placeholder="Poster URL" type="text" value={newMovie.poster} onChange={(event) => setNewMovie({...newMovie, poster: event.target.value})} />
                 <input required placeholder="Movie Description" type="text" value={newMovie.description} onChange={(event) => setNewMovie({...newMovie, description: event.target.value})}/>
@@ -35,6 +52,8 @@ function AddMovie() {
                         return(<option key={genre.id} value={genre.id} onClick={(event) => setNewMovie({...newMovie, genres: [...newMovie.genres, event.target.value]})}>{genre.name}</option>);
                     })}
                 </select>
+                <button onClick={handleBack}>Back</button>
+                <button type="submit">Save</button>
             </form>
         </>
     );
