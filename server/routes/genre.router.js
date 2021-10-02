22576ea000fe97460c8d2b8b8bfc4947417b5924
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-02 09:04:10 -0400

    genres.router - Added GET method to retrieve genres from DB and return to client

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -3,8 +3,15 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
+  const queryText = `SELECT * FROM "genres" ORDER BY "name" ASC`;
+  pool.query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log('ERROR: Get all genres', error);
+      res.sendStatus(500);
+    })
 });
 
 module.exports = router;
