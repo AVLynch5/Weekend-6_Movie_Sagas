f6b5198b0d7723a43381fcf4d7ba72194c7e45b3
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-03 17:23:02 -0400

    movielist - added history. goToDetails routes user to details page

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 import './MovieList.css'
 
 function MovieList() {
-
+    const history = useHistory();
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
 
@@ -15,6 +16,7 @@ function MovieList() {
     const goToDetails = (movieID) => {
         dispatch({type: 'FETCH_MOVIE_DETAILS', payload: movieID});
         //console.log('The movieID is', movieID);//test
+        history("/details");
     }
 
     return (
