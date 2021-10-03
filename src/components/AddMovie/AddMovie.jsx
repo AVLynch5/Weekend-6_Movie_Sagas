cf5a82cd4de28f41443cc7ecc428561281c8fba4
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-03 12:29:25 -0500

    updated Addmovie with function checkGenreArray to prevent multiple entry. Also added Menu option to dropdown

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -28,6 +28,23 @@ function AddMovie() {
         history.push("/");
     }
 
+    //function checkGenreArray - function to check newMovie.genres for prior instance of genreID. Prevents duplicate entries.
+    const checkGenreArray = (event) => {
+        const idToCheck = event.target.value;
+        //if user tries to select initial menu as genre, do not add to genre array
+        if (idToCheck == 'MENU') {
+            return;
+        }
+        //if idToCheck exists within newMovie.genres, do not add to genre array
+        for (let id of newMovie.genres) {
+            if (id == idToCheck) {
+                return;
+            }
+        }
+        //add genre id to newMovie genres array
+        setNewMovie({...newMovie, genres: [...newMovie.genres, event.target.value]})
+    }
+
     //function addNewMovie - dispatch to call POST, clear inputs, return user to home
     const addNewMovie = (event) => {
         //prevent default form behavior
@@ -53,7 +70,8 @@ function AddMovie() {
                 <input required placeholder="Movie Title" type="text" value={newMovie.title} onChange={(event) => setNewMovie({...newMovie, title: event.target.value})} />
                 <input required placeholder="Poster URL" type="text" value={newMovie.poster} onChange={(event) => setNewMovie({...newMovie, poster: event.target.value})} />
                 <input required placeholder="Movie Description" type="text" value={newMovie.description} onChange={(event) => setNewMovie({...newMovie, description: event.target.value})}/>
-                <select required name="genresDropdown" label="Select Genres" onChange={(event) => setNewMovie({...newMovie, genres: [...newMovie.genres, event.target.value]})}>
+                <select required name="genresDropdown" label="Select Genres" onChange={(event) => checkGenreArray(event)}>
+                    <option value='MENU'>Menu</option>
                     {genreList.map((genre) => {
                         return(<option key={genre.id} value={genre.id}>{genre.name}</option>);
                     })}
