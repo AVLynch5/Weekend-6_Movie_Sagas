4cd760581a7d5e1a68ce0be4cb535e97918b644b
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-06 00:02:15 -0400

    Added MUI styling to MovieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css';
 import MovieItem from '../MovieItem/MovieItem';
+import { Box, Grid } from '@material-ui/core';
 
 function MovieList() {
     const dispatch = useDispatch();
@@ -13,12 +14,21 @@ function MovieList() {
 
     return (
         <main>
-            <h1>MovieList</h1>
-            <section className="movies">
+            <Box>
+                <h2>Here's what's Showing:</h2>
+            </Box>
+            <Grid
+                className="movies"
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+            >
                 {movies.map(movie => {
-                    return (<MovieItem movie={movie} />);
+                    return (<MovieItem key={movie.id} movie={movie} />);
                 })}
-            </section>
+            </Grid>
         </main>
 
     );
