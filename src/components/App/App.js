e471caf009b5237bfb75ca85bcb7e532da82e21d
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-05 23:01:24 -0400

    App.jsx - wrapped app contents w/in container + Paper. Made header a component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,26 +3,31 @@ import './App.css';
 import MovieList from '../MovieList/MovieList'
 import AddMovie from '../AddMovie/AddMovie';
 import Details from '../Details/Details';
+import { Container } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
+import Header from '../Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>       
-        <Route path="/" exact>
-          <Link to="/addmovie">Add a Movie</Link> 
-          <MovieList />
-        </Route>
-        {/* Details page */}
-        <Route path="/details/:movieid">
-          <Details />
-        </Route>
-        {/* Add Movie page */}
-        <Route path="/addmovie">
-          <AddMovie />
-        </Route>
-      </Router>
-    </div>
+    <Container className="App" >
+      <Paper>
+        <Header />
+        <Router>       
+          <Route path="/" exact>
+            <Link to="/addmovie">Add a Movie</Link> 
+            <MovieList />
+          </Route>
+          {/* Details page */}
+          <Route path="/details/:movieid">
+            <Details />
+          </Route>
+          {/* Add Movie page */}
+          <Route path="/addmovie">
+            <AddMovie />
+          </Route>
+        </Router>
+      </Paper>
+    </Container>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return(
+        <>
+            <header>
+                <h1>The Movies Saga!</h1>
+            </header>
+        </>
+    );
+}
+
+export default Header;
