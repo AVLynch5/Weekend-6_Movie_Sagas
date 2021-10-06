b01df808d42a132f12a0ee20ee940beb6d4f6166
Anthony Lynch <anthonyvlynch5@gmail.com>
2021-10-06 13:16:34 -0400

    AddMove - commented out JSON.stringify and restructured form

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { useState } from "react";
 import { useHistory } from "react-router";
+import { TextareaAutosize } from "@material-ui/core";
 
 function AddMovie() {
     const history = useHistory();
@@ -62,20 +63,23 @@ function AddMovie() {
 
     return(
         <>
-            {JSON.stringify(genreList)}
-            <br/>
-            {JSON.stringify(newMovie)}
+            {/*JSON.stringify(genreList)*/}
+            {/*<br/>*/}
+            {/*JSON.stringify(newMovie)*/}
             <h3>Add a new movie:</h3>
             <form onSubmit={addNewMovie}>
                 <input required placeholder="Movie Title" type="text" value={newMovie.title} onChange={(event) => setNewMovie({...newMovie, title: event.target.value})} />
                 <input required placeholder="Poster URL" type="text" value={newMovie.poster} onChange={(event) => setNewMovie({...newMovie, poster: event.target.value})} />
-                <input required placeholder="Movie Description" type="text" value={newMovie.description} onChange={(event) => setNewMovie({...newMovie, description: event.target.value})}/>
+                <br/>
+                <textarea required placeholder="Movie Description" type="text" value={newMovie.description} onChange={(event) => setNewMovie({...newMovie, description: event.target.value})}/>
+                <br/>
                 <select required name="genresDropdown" label="Select Genres" onChange={(event) => checkGenreArray(event)}>
                     <option value='MENU'>Menu</option>
                     {genreList.map((genre) => {
                         return(<option key={genre.id} value={genre.id}>{genre.name}</option>);
                     })}
                 </select>
+                <br/>
                 <button onClick={handleBack}>Back</button>
                 <button type="submit">Save</button>
             </form>
